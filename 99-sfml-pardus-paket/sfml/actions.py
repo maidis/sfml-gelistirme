12819301943e8cac13283f34e2a3ae827a359e83
maidis <ozbekanil@gmail.com>
2011-04-21 13:53:48 +0300

    sync with git

--- 99-sfml-pardus-paket/sfml/actions.py
+++ 99-sfml-pardus-paket/sfml/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "LaurentGomila-SFML-cb1f938"
+WorkDir = "LaurentGomila-SFML-17971ca"
 
 def setup():
     cmaketools.configure()
